76517ae0db040607c52bf3a423a1de7e23ce7422
Sungmin Cha <csm9493@gmail.com>
2021-10-07 04:43:34 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -10,8 +10,3 @@ CUDA_VISIBLE_DEVICES=$GPU python main_gan2gan.py --dataset $DATASET --mbs 128 --
 DATASET='Dose25'   #['Dose25','Dose50','Dose75','Dose100']
 CUDA_VISIBLE_DEVICES=$GPU python main_gan.py --dataset $DATASET --save-only-final-weights
 CUDA_VISIBLE_DEVICES=$GPU python main_gan2gan.py --dataset $DATASET --mbs 4 --iter 3 --print-only-final-ep
-
-# Train Generative Models for Medical dataset
-DATASET='Mixture_std15'   #['Mixture_std15','Mixture_std25', 'Mixture_std30', 'Mixture_std50']
-CUDA_VISIBLE_DEVICES=$GPU python main_gan.py --dataset $DATASET --save-only-final-weights
-CUDA_VISIBLE_DEVICES=$GPU python main_gan2gan.py --dataset $DATASET --mbs 4 --iter 3 --print-only-final-ep
